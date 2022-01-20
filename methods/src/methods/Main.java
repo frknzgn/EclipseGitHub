f3703f2bb92f6e9b11d8c7f0939e62fee5fb986c
Furkan Özgün <frknzgn9@gmail.com>
2022-01-20 20:22:10 +0300

    methods

---
+++ methods/src/methods/Main.java
@@ -0,0 +1,20 @@
+package methods;
+
+//PascalCase
+public class Main {
+	
+	public static void main(String[] args) {
+		int result = sum(1,2)*3;
+		System.out.println(result);
+	}
+	
+	//camelCase
+	public static int sum(int number1,int number2) {
+		
+		return number1+number2;
+		
+	}
+	
+	
+
+}
